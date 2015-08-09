91a1961c1bad5d13d8adcddaccfd098165fdc588
Nicholas Scheel <nasalmusician@gmail.com>
2015-08-09 14:01:45 +0000

    Indentation

--- PHP5/user/classes.php
+++ PHP5/user/classes.php
@@ -10,17 +10,18 @@ global $mysqli;
 
 $stmt = $mysqli->prepare("
 	SELECT class_id, class_name FROM classes
+	WHERE NOT hidden
 ");
-			if (!$stmt) {
-				_die("Prepare failed (".__FILE__."@".__LINE__."): (" . $stmt->errno . ") " . $stmt->error);
-				return $result;
-			}
-			$stmt->bind_result($id, $name);
-			if (!$stmt->execute()) {
-				_die("Execute failed (".__FILE__."@".__LINE__."): (" . $stmt->errno . ") " . $stmt->error);
-				return $result;
-			}
-			while ($stmt->fetch()) {
-				?> <label><input name="signupclass" type="radio" value="<?= $id ?>"><?= $name ?></label><br> <?php
-			}
+if (!$stmt) {
+	_die("Prepare failed (".__FILE__."@".__LINE__."): (" . $stmt->errno . ") " . $stmt->error);
+	return $result;
+}
+$stmt->bind_result($id, $name);
+if (!$stmt->execute()) {
+	_die("Execute failed (".__FILE__."@".__LINE__."): (" . $stmt->errno . ") " . $stmt->error);
+	return $result;
+}
+while ($stmt->fetch()) {
+	?> <label><input name="signupclass" type="radio" value="<?= $id ?>"><?= $name ?></label><br> <?php
+}
 ?>
