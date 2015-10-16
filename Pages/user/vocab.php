a17efda89784a52fc107c572b6e16385d9a53278
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-10-15 21:06:48 -0500

    Removed all references to #46A, deleted unnecessary nav.js

--- Pages/user/vocab.php
+++ Pages/user/vocab.php
@@ -24,7 +24,7 @@
         uvtable.setControlIDs('uvUControls', 'uvLControls');
         uvtable.setUControls(['new', 'edit', 'refresh', 'search']);
         uvtable.setLControls(['new', 'edit', 'refresh', 'search']);
-        uvtable.setTColumns([['table-checkbox'], ['ID', 0, 'number'], ['Name', 1, 'text'], ['Part of speech', 2, 'drop-down', [['N.', 'noun'], ['Adj.', 'adjective'], ['V.', 'verb'], ['Adv.', 'adverb'], ['Pro.', 'pronoun']]], ['jcanvas', 'Delete', [20, 20], 'cs,b,m:1:1,l:5:19,l:15:19,l:20:1,l:1:1,m:7:1,l:7:19,m:13:1,l:13:19,m:2:6,l:18:6,m:3:13,l:17:13,m:0:0,ss:#4466AA,w:2,c,s', 'table-delete'], ['jcanvas', 'Edit', [20, 20], ['cs,b,m:20:20,l:6:6,l:0:3,m:6:6,l:3:0,m:0:20,l:14:6,l:17:0,m:14:6,l:20:3,ss:#4466AA,w:2,c,s', 'cs,b,m:1:1,l:1:19,l:19:19,l:19:5,l:15:1,l:1:1,m:15:1,l:15:5,l:19:5,ss:#4466AA,w:2,c,s'], 'table-edit']]);
+        uvtable.setTColumns([['table-checkbox'], ['ID', 0, 'number'], ['Name', 1, 'text'], ['Part of speech', 2, 'drop-down', [['N.', 'noun'], ['Adj.', 'adjective'], ['V.', 'verb'], ['Adv.', 'adverb'], ['Pro.', 'pronoun']]], ['jcanvas', 'Delete', [20, 20], 'cs,b,m:1:1,l:5:19,l:15:19,l:20:1,l:1:1,m:7:1,l:7:19,m:13:1,l:13:19,m:2:6,l:18:6,m:3:13,l:17:13,m:0:0,ss:#A00,w:2,c,s', 'table-delete'], ['jcanvas', 'Edit', [20, 20], ['cs,b,m:20:20,l:6:6,l:0:3,m:6:6,l:3:0,m:0:20,l:14:6,l:17:0,m:14:6,l:20:3,ss:#A00,w:2,c,s', 'cs,b,m:1:1,l:1:19,l:19:19,l:19:5,l:15:1,l:1:1,m:15:1,l:15:5,l:19:5,ss:#A00,w:2,c,s'], 'table-edit']]);
         uvtable.setKeyField(0);
         uvtable.setBindHandler(function() {
             for (var drpos in this.drows) {
