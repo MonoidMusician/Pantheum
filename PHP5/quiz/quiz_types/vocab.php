ab879d19e41d2ce3563d4110bd0b511a6d732436
Nicholas Scheel <nasalmusician@gmail.com>
2015-10-21 16:49:26 +0000

    Stage-appropriate vocab

--- PHP5/quiz/quiz_types/vocab.php
+++ PHP5/quiz/quiz_types/vocab.php
@@ -208,6 +208,142 @@ $quiz_types = array_merge($quiz_types,[
 			"choices0-tooltip"=>"Pick correct definition",
 		]]
 	],
+	"random-definitions-stage28-29-mc" => [
+		"name" => "Stage 28–29 Vocabulary (MC)",
+		"category" => "Vocabulary",
+		"lang" => "la",
+		"options" => [[
+			"help" => "Choose a correct definition for the given word",
+			"selections" => [
+				"word"=>function($_, $db, $path) {
+					$s = $db->searcher();
+					$s->stmt .= "
+						WHERE word_id IN (
+							SELECT word_id FROM definitions
+							WHERE def_lang = 'en'
+							AND def_type IS NULL
+						)
+						AND word_lang = 'la'
+						AND word_id NOT IN (
+							SELECT word_id FROM attributes
+							WHERE attr_tag = 'template' OR attr_tag = 'hidden'
+						)
+						AND (
+							SELECT attr_value FROM attributes
+							WHERE word_id = words.word_id
+							AND attr_tag = 'clc-stage' 
+						) IN (28,29)";
+					$s->args = [];
+					return $s->rand();
+				},
+			],
+			"sentence" => [
+				$OP_LQUOTE,
+				function($pick_db) { return format_word($pick_db["word"]->name()); },
+				$OP_RQUOTE, $OP_COLON,
+				$OP_MULTIPLE_CHOICE,
+			],
+			"choices0-language" => "en",
+			"choices0" => function($pick_db, $db) {
+				global $mysqli;
+				$query = $mysqli->prepare("
+					SELECT DISTINCT def_id FROM definitions
+					WHERE def_lang = 'en'
+					AND word_id = (?)
+					AND def_type IS NULL
+					ORDER BY rand()
+					LIMIT 1
+				");
+				$res0 = NULL;
+				sql_getmany($query, $res0, ["i",$pick_db["word"]->id()]);
+				$query->close();
+				if (!$res0) return NULL;
+				$query = $mysqli->prepare("
+					SELECT DISTINCT def_id FROM definitions
+					WHERE def_lang = 'en'
+					AND def_type IS NULL
+					AND def_id != (?)
+					AND def_value != ''
+					AND word_id in (
+						SELECT word_id FROM words WHERE word_lang = 'la'
+					)
+					AND word_id NOT IN (
+						SELECT word_id FROM attributes
+						WHERE attr_tag = 'template' OR attr_tag = 'hidden'
+					)
+				");
+				$res1 = NULL;
+				sql_getmany($query, $res1, ["i", $res0[0]]);
+				$query->close();
+				if (!$res1) return NULL;
+				$res1 = choose_n_unique($res1, 4);
+				$res = array_merge($res0, $res1);
+				foreach ($res as &$r) $r=definition(defaultDB(), $r);
+				foreach ($res as &$r) $r=[
+					"correct" => $r->word()->id() === $pick_db["word"]->id(),
+					"value" => ((string)$r->path() ? "(".$r->path().") " : "").str_replace("\n", ", ", $r->value()),
+				];
+				return $res;
+			},
+			"choices0-tooltip"=>"Pick correct definition",
+		]]
+	],
+	"random-definitions-stage28-29-fr" => [
+		"name" => "Stage 28–29 Vocabulary (FR)",
+		"category" => "Vocabulary",
+		"lang" => "la",
+		"options" => [[
+			"help" => "Choose a correct definition for the given word",
+			"selections" => [
+				"word"=>function($_, $db, $path) {
+					$s = $db->searcher();
+					$s->stmt .= "
+						WHERE word_id IN (
+							SELECT word_id FROM definitions
+							WHERE def_lang = 'en'
+							AND def_type IS NULL
+						)
+						AND word_lang = 'la'
+						AND word_id NOT IN (
+							SELECT word_id FROM attributes
+							WHERE attr_tag = 'template' OR attr_tag = 'hidden'
+						)
+						AND (
+							SELECT attr_value FROM attributes
+							WHERE word_id = words.word_id
+							AND attr_tag = 'clc-stage' 
+						) IN (28,29)";
+					$s->args = [];
+					return $s->rand();
+				},
+			],
+			"sentence" => [
+				$OP_LQUOTE,
+				function($pick_db) { return format_word($pick_db["word"]->name()); },
+				$OP_RQUOTE, $OP_COLON,
+				$OP_USER_INPUT,
+			],
+			"answer0-language" => "en",
+			"answer0" => function($pick_db, $db) {
+				global $mysqli;
+				$query = $mysqli->prepare("
+					SELECT DISTINCT def_id FROM definitions
+					WHERE def_lang = 'en'
+					AND def_type IS NULL
+					AND word_id = (?)
+				");
+				$res = NULL;
+				sql_getmany($query, $res, ["i",$pick_db["word"]->id()]);
+				$query->close();
+				if (!$res) return NULL;
+				$ret = []; $ret2 = [];
+				foreach ($res as $r) $ret = array_merge($ret, explode("\n",definition(defaultDB(), $r)->value()));
+				foreach ($ret as $r) $ret2 = array_merge($ret2, array_map("trim",explode(",", $r)));
+				return ["correct"=>explode(",",$ret[0]),"acceptable"=>$ret2];
+			},
+			"answer0-tooltip"=>"Type correct definition",
+		]]
+	],
 	"009" => [
 		"name" => "Synonyms",
 		"lang" => "la",
