ef4d0b8a54a1d7677ea028d10c5b7ed5d56abfd5
Nicholas Scheel <nasalmusician@gmail.com>
2015-10-23 21:02:22 +0000

    Update index page translations

--- Pages/home/home-logged-in.php
+++ Pages/home/home-logged-in.php
@@ -13,12 +13,12 @@
         <button data-i18n="link.latin_websites" class="large" onclick="window.location.href='/links.php';">Latin websites</button><br>
         <button data-i18n="link.sentence_viewer" class="large" onclick="window.location.href='/sentence.php';">Sentence Viewer</button><br>
         <button data-i18n="link.dictionary" class="large" onclick="window.location.href='/dictionary.php';">Dictionary</button><br>
-        <button data-i18n="link.dictionary" class="large" onclick="window.location.href='/user.php';">Account Settings</button><br>
-        <?php if (requireRank(1, FALSE)) { ?>
-        <button data-i18n="link.add_words" class="large" onclick="window.location.href='/add_word.php';">Admin Settings</button><br>
-        <?php } ?>
         <?php if (requireRank(3, FALSE)) { ?>
         <button data-i18n="link.add_words" class="large" onclick="window.location.href='/add_word.php';">Add words</button><br>
+        <?php } ?>
+        <button data-i18n="link.settings" class="large" onclick="window.location.href='/user.php';">Account Settings</button><br>
+        <?php if (requireRank(1, FALSE)) { ?>
+        <button data-i18n="link.admin" class="large" onclick="window.location.href='/add_word.php';">Admin Settings</button><br>
         <?php } ?>
         <br>
         <span data-i18n="msg.loggedin">Thanks for logging in, your answers will be saved.</span>
