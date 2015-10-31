9cb7242f68478b139b67d8b181e25354b8655ceb
Nicholas Scheel <nasalmusician@gmail.com>
2015-10-31 21:33:26 +0000

    Google Analytics

--- Includes/js.php
+++ Includes/js.php
@@ -6,6 +6,15 @@
         <script>
             var pantheum = {udata:<?= $sudata ? $sudata : 'null' ?>,_private:{}};
         </script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-69525223-1', 'auto');
+            ga('send', 'pageview');
+        </script>
         <script type="text/javascript" src="/JS/lib/jquery.js"></script>
         <script type="text/javascript" src="/JS/lib/jCanvas.js"></script>
         <script type="text/javascript" src="/JS/lib/jTable.js"></script>
