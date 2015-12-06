75ad4d07da943e62b0ca423b67c282dc3e050699
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-12-06 13:59:31 -0600

    Fixed to flush buffer automatically.

--- httptest.php
+++ httptest.php
@@ -1,6 +1,21 @@
-<?php
-for ($i = 0; $i < 1000; $i++) {
-echo "a";
-sleep(1);
+<?php 
+@ini_set("output_buffering", "Off");
+@ini_set('implicit_flush', 1);
+@ini_set('zlib.output_compression', 0);
+@ini_set('max_execution_time',1200);
+
+
+header( 'Content-type: text/html; charset=utf-8' );
+for ($i = 0; $i < 3600; $i++) {
+    echo "a";
+
+    if(sleep(1)!=0)
+    {
+        echo "b"; 
+        break;
+    }
+    flush();
+    ob_flush();
 }
+
 ?>
