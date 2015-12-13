bbc88ef118d2bc455edeeada2b127d3cfa6676eb
Nicholas Scheel <nasalmusician@gmail.com>
2015-12-13 22:23:36 +0000

    Work on tools.php
    * Style the Google search box using their style
    * Add timezone support (using Google's API)
    * Show the length of a Roman hour

--- Pages/tools.php
+++ Pages/tools.php
@@ -129,7 +129,7 @@ $('#roman-number') .on('keyup', change[1]).on('keydown', change[1]);
 
 <article>
 <h2>Roman time</h2>
-<br>
+<p>Roman hours split the solar day (sunrise to sunset) into 12 equal parts. Find sunrise/sunset times for a date and location.
 
 <div>
 	<h3>Date</h3>
@@ -142,12 +142,44 @@ $('#roman-number') .on('keyup', change[1]).on('keydown', change[1]);
 <div>
 	<h3>Location</h3>
 	<input id="place">
+	<style>
+		#place {
+			background-color: #fff;
+			font-family: Roboto;
+			font-size: 15px;
+			font-weight: 300;
+			margin-left: 12px;
+			padding: 0 11px 0 13px;
+			text-overflow: ellipsis;
+			width: 300px;
+			margin-top: 10px;
+			border: 1px solid transparent;
+			border-radius: 2px 0 0 2px;
+			box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			height: 32px;
+			outline: none;
+			box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+		}
+
+		input#place:focus:not(.select2-search__field) {
+			border-color: #4d90fe;
+		}
+	</style>
 	<input class="medium" id="latitude" placeholder="Latitude"><input class="medium" id="longitude" placeholder="Longitude">
 	<button id="to-rome">In Rome</button>
 	<button id="to-london">In London</button>
 	<button id="hither">WhereAmI</button>
 
 	<div id="map" style="height: 300px;"></div>
+</div>
+
+<div>
+	<h3>Timezone</h3>
+	UTC+<input id="timezone" class="small" value="0" min="-12" max="12" type="number">
+	<button id="utc">UTC</button>
+	<button id="localtime">My Timezone</button>
+	<button id="loctime">By location</button>
 </div>
 
 <div>
@@ -158,6 +190,7 @@ $('#roman-number') .on('keyup', change[1]).on('keydown', change[1]);
 		<span id="noon"></span><br>
 	Sunset:
 		<span id="sunset"></span><br>
+	Roman hour: <span id="romanhour"></span> hours / <span id="romanminutes"></span> minutes
 </div>
 
 <br>
@@ -172,44 +205,22 @@ var dateToStr = function(d) {
 	return d3.time.format.iso(d).split("T")[0];
 };
 
-var date = new Date(), lat = 0, lng = 0;
+var date = new Date(), lat = 0, lng = 0, timezone = 0;
 date.setUTCDate(date.getDate());
 
-// Calendar
-var calendar = $('#calendar').calendar({date:date}).on('click', function() {
-	$('input#date').val($(this).data('date'));
-	date = new Date($(this).data('date'));
-	displaytimes(date, +lat, +lng);
-});
-var update_date = function(d) {
-	date = d; var s = dateToStr(date);
-	$('input#date').val(s);
-	calendar.data('date', s).update(date);
-	displaytimes(date, +lat, +lng);
-};
-$('#today').on('click', function() {
-	var d = new Date();
-	d.setUTCDate(d.getDate());
-	update_date(d);
-});
-$('#romefounding').on('click', function() {
-	update_date(new Date("-000753-04-21"));
-});
-$('#date').on('change', function() {
-	var d = new Date($(this).val());
-	if (d)
-		update_date(d);
-});
 
+
+// Output graph
 var chart = d3.select('#solarchart').chart('Compose', function(data) {
 	var scales = {
-		x: {data: data, key: 'x'},
-		y: {domain: [0, 24]}
+		x: {data: data.data, key: 'x'},
+		y: {domain: data.range}
+		//y: {data: data, key: 'y'},
 	};
 
 	var charts = [
 		d3c.lines('results', {
-			data: data,
+			data: data.data,
 			xScale: scales.x,
 			yScale: scales.y
 		})
@@ -231,14 +242,41 @@ var chart = d3.select('#solarchart').chart('Compose', function(data) {
 }).width(800).height(500);
 
 
-function convertDateToUTC(date) { return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); }
 function displaytimes(date, lat, lng) {
 	var times = SunCalc.getTimes(date, lat, lng);
 	$('#latitude').val(lat); $('#longitude').val(lng);
-	var f = d3.time.format.utc("%0I:%M %p (UTC)");
+	var _f = d3.time.format.utc("%0H:%M"), f = function(d) {
+		if (isNaN(d.getTime())) return "None";
+		d = new Date(d);
+		d.setHours(d.getHours() + timezone);
+		return _f(d);
+	};
+	function fix(t, d, h) {
+		if (!isNaN(t.getTime())) return t;
+		t = new Date(d);
+		t.setUTCHours(h);
+		return t;
+	}
+	function getT(t, prev) {
+		if (Array.isArray(prev)) {
+			if (!prev.length) prev = getT(t);
+			else prev = prev[prev.length-1];
+		}
+		if (typeof prev === 'object')
+			prev = prev.y;
+		if (isNaN(t.getTime()))
+			return prev;
+		var t = t.getUTCHours() + t.getUTCMinutes()/60;
+		if (prev === undefined) return t;
+		if (t > prev+6) t -= 24;
+		else if (t < prev-6) t += 24;
+		return t;
+	}
 	$('#sunrise').text(f(times.sunrise));
 	$('#sunset' ).text(f(times.sunset));
 	$('#noon'   ).text(f(times.solarNoon));
+	$('#romanhour'   ).text(((getT(times.sunset)-getT(times.sunrise))/12).toFixed(2));
+	$('#romanminutes').text(((getT(times.sunset)-getT(times.sunrise))/12*60).toFixed(0));
 
 
 	var data = [
@@ -270,44 +308,105 @@ function displaytimes(date, lat, lng) {
 		values: []
 	  }
 	];
-	var year = date.getYear();
+	var year = date.getFullYear();
 	var s = new Date(year, 0, 1), e = new Date(year, 12, 1);
 	var i = 0; var avgnoon = 0;
+	var _t4 = undefined, _t5 = undefined;
 	for (var d = new Date(s); d <= e; d.setDate(d.getDate() + 1)) {
 		i++;
 		var times = SunCalc.getTimes(d, lat, lng);
 		var t1 = times.sunrise;
-		data[0].values.push({x:i,y:t1.getUTCHours() + t1.getUTCMinutes()/60});
+		data[0].values.push({x:i,y:getT(t1, data[0].values)});
 		var t2 = times.sunset;
-		data[1].values.push({x:i,y:t2.getUTCHours() + t2.getUTCMinutes()/60});
+		data[1].values.push({x:i,y:getT(t2, data[1].values)});
 		var t3 = times.solarNoon;
-		data[2].values.push({x:i,y:t3.getUTCHours() + t3.getUTCMinutes()/60});
-		var t4 = 0.5*t1 + 0.5 * t3; t4 = new Date(t4);
-		data[3].values.push({x:i,y:t4.getUTCHours() + t4.getUTCMinutes()/60});
-		var t5 = 0.5*t2 + 0.5 * t3; t5 = new Date(t5);
-		data[4].values.push({x:i,y:t5.getUTCHours() + t5.getUTCMinutes()/60});
+		data[2].values.push({x:i,y:getT(t3, data[2].values)});
+		var t4 = 0.5*t1 + 0.5 * t3; t4 = getT(new Date(t4), _t4);
+		data[3].values.push({x:i,y:t4});
+		var t5 = 0.5*t2 + 0.5 * t3; t5 = getT(new Date(t5), _t5);
+		data[4].values.push({x:i,y:t5});
 		avgnoon += times.solarNoon.getUTCHours();
+		_t4 = t4; _t5 = t5;
 	}
 	avgnoon /= (i-1);
-	var timezone = Math.round(36 - avgnoon);
+	var tz = Math.round(12 - avgnoon);
 	$.each(data, function(i,d) {
 		$.each(d.values, function(j,pt) {
 			pt.y += timezone;
-			pt.y = pt.y % 24;
 		});
 	});
 
-	chart.draw(data);
+	chart.draw({data:data, range:[-tz+timezone,-tz+timezone+24]});
 }
 
+
+
+
+
+
+
+// Timezone
+$('#utc').on('click', function() {
+	$('#timezone').val(0).trigger('change');
+});
+$('#localtime').on('click', function() {
+	$('#timezone').val(-(new Date()).getTimezoneOffset()/60).trigger('change');
+});
+$('#loctime').on('click', function() {
+	$.get('https://maps.googleapis.com/maps/api/timezone/json', {
+		location: lat+','+lng,
+		timestamp: date.getTime()/1000,
+		key: 'AIzaSyB2kxU6e0-_Wqgaac-IJ7ZI5X1gEaG6IsE',
+	}).done(function(data) {
+		console.log(data);
+		if (data.status != "OK")
+			alert(data.status);
+		$('#timezone').val((data.dstOffset + data.rawOffset)/60/60).trigger('change');
+	});
+});
+$('#timezone').on('change', function() {
+	timezone = +$(this).val();
+	displaytimes(date, lat, lng);
+});
+
+// Calendar
+var calendar = $('#calendar').calendar({date:date}).on('click', function() {
+	$('input#date').val($(this).data('date'));
+	date = new Date($(this).data('date'));
+	displaytimes(date, +lat, +lng);
+});
+var update_date = function(d) {
+	date = d; var s = dateToStr(date);
+	$('input#date').val(s);
+	calendar.data('date', s).update(date);
+	displaytimes(date, +lat, +lng);
+};
+$('#today').on('click', function() {
+	var d = new Date();
+	d.setUTCDate(d.getDate());
+	update_date(d);
+});
+$('#romefounding').on('click', function() {
+	update_date(new Date("-000753-04-21"));
+});
+$('#date').on('change', function() {
+	var d = new Date($(this).val());
+	if (d)
+		update_date(d);
+});
+
 // Location
-function update_map(latitude, longitude) {
+function update_map(latitude, longitude, searching) {
 	lat = +latitude, lng = +longitude;
 	$('#latitude').val(lat);
 	$('#longitude').val(lng);
 	if (typeof map != 'undefined') {
 		map.setCenter({lat:lat,lng:lng});
 		marker.setPosition({lat:lat,lng:lng});
+		if (searching != true) {
+			if (!searching) searching = '';
+			$(input).val(searching);
+		}
 	}
 	displaytimes(date, lat, lng);
 }
@@ -316,8 +415,8 @@ $('#latitude, #longitude').on('change', function() {
 	if (lat == +lat && lng == +lng)
 		update_map(lat, lng);
 });
-$('#to-rome'  ).on('click', function() {update_map(41.9,12.5);}).trigger('click');
-$('#to-london').on('click', function() {update_map(51.5,-0.1);});
+$('#to-rome'  ).on('click', function() {update_map(41.90278349999999,12.496365500000024,"Rome");}).trigger('click');
+$('#to-london').on('click', function() {update_map(51.5073509,-0.12775829999998223,"London");});
 if ("geolocation" in navigator) {
 	$('#hither').on('click', function() {
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -331,7 +430,7 @@ if ("geolocation" in navigator) {
 
 update_date(date);
 
-var map, marker;
+var map, marker, input, searchBox;
 initMap = function() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: lat, lng: lng},
@@ -346,8 +445,8 @@ initMap = function() {
         update_map(marker.getPosition().lat(),marker.getPosition().lng());
     });
 
-	var input = document.getElementById('place');
-	var searchBox = new google.maps.places.SearchBox(input);
+	input = document.getElementById('place');
+	searchBox = new google.maps.places.SearchBox(input);
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 	// Bias the SearchBox results towards current map's viewport.
@@ -373,7 +472,7 @@ initMap = function() {
 
 		if (places.length == 1) {
 			var loc = places[0].geometry.location;
-	        update_map(loc.lat(),loc.lng());
+	        update_map(loc.lat(),loc.lng(),true);
 			return;
 		}
 
