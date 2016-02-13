9dda4abee666d5c4c9cb376d0492dc3e9d715e07
Nicholas Scheel <nasalmusician@gmail.com>
2016-02-13 20:34:39 +0000

    Split interface/input CSS into new file
    Add round button classes for help buttons.

--- Includes/css.php
+++ Includes/css.php
@@ -1,3 +1,4 @@
 		<link rel="stylesheet" type="text/css" href="/CSS/main.css">
+		<link rel="stylesheet" type="text/css" href="/CSS/interface.css">
 		<link rel="stylesheet" type="text/css" href="/CSS/jquery.qtip.min.css">
 		<link rel="stylesheet" type="text/css" href="/CSS/jTable.css">
