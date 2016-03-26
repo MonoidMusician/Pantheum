8f1cb36d4de48cf9ce4583db0ab4d20a7ecc4461
Nicholas Scheel <nasalmusician@gmail.com>
2016-03-26 03:15:35 +0000

    Rename *.jsx to *.js
    Still use babel for ...arg notation.

--- Includes/jsx.php
+++ Includes/jsx.php
@@ -2,6 +2,6 @@
 		<script type="text/javascript" src="/JS/react/react.js"></script>
 		<script type="text/javascript" src="/JS/react/react-dom.js"></script>
 		<script type="text/javascript" src="/JS/react/react-hyperscript-include.min.js"></script>
-		<script type="text/babel" src="/JS/react/view.jsx"></script>
-		<script type="text/babel" src="/JS/react/attributes.jsx"></script>
-		<script type="text/babel" src="/JS/react/dictionary.jsx"></script>
+		<script type="text/babel" src="/JS/react/view.js"></script>
+		<script type="text/babel" src="/JS/react/attributes.js"></script>
+		<script type="text/babel" src="/JS/react/dictionary.js"></script>
