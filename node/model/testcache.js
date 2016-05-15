4df7234d3abd38e2355508325206746dee60c888
Nicholas Scheel <nasalmusician@gmail.com>
2016-05-15 19:33:57 +0000

    Remove useless model/test*

--- node/model/testcache.js
+++
@@ -1,9 +0,0 @@
-var connection = require('./mysql');
-var Definition = require('./model').Definition;
-
-var D = Definition({id:19}, true);
-console.log('D:', D, Object.getPrototypeOf(D));
-var D2 = Definition({id:19, cacheable:true});
-console.log('Equal?', D2 === D);
-var D3 = Definition(D2);
-console.log('Equal?', D3 === D);
