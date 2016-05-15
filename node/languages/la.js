cf8b96203bbc7abfe45a0b4202c34548e1f4c7fd
Nicholas Scheel <nasalmusician@gmail.com>
2016-05-15 19:57:12 +0000

    Extra modules

---
+++ node/languages/la.js
@@ -0,0 +1,80 @@
+var model = require('../model');
+
+var parts = {
+	"number": ["singular","plural"],
+	"case":["vocative","nominative","accusative","ablative","dative","genitive","locative"],
+	"gender":["feminine","masculine","neuter"],
+	"person":["person-1","person-2","person-3"],
+	"voice":["active","passive"],
+	"tense":["present","imperfect","future","perfect","pluperfect","future-perfect"]
+};
+model.Depath.add("la", "verb", new model.Depath("verb", {"mood":{
+	"indicative":{
+		"voice": parts["voice"],
+		"tense": parts["tense"],
+		"number": parts["number"],
+		"person": parts["person"],
+		"gender": parts["gender"]
+	},
+	"subjunctive":{
+		"voice": parts["voice"],
+		"tense":[
+			"present",
+			"imperfect",
+			"perfect",
+			"pluperfect"
+		],
+		"number": parts["number"],
+		"person": parts["person"],
+		"gender": parts["gender"]
+	},
+	"imperative":{
+		"voice": parts["voice"],
+		"tense":[
+			"present",
+			"future"
+		],
+		"number": parts["number"],
+		"person": parts["person"],
+		"gender": parts["gender"]
+	},
+	"participle":{
+		"voice": parts["voice"],
+		"tense":[
+			"present",
+			"perfect",
+			"future"
+		],
+		"number": parts["number"],
+		"gender": parts["gender"],
+		"case": parts["case"]
+	},
+	"infinitive":{
+		"voice": parts["voice"],
+		"tense":[
+			"present",
+			"perfect",
+			"future"
+		],
+		"number": parts["number"],
+		"gender": parts["gender"],
+		"case":[
+			"nominative",
+			"accusative"
+		]
+	},
+	"gerund":{
+		"case":[
+			"accusative",
+			"ablative",
+			"dative",
+			"genitive"
+		]
+	},
+	"supine":{
+		"case":[
+			"accusative",
+			"ablative"
+		]
+	}
+}}));
