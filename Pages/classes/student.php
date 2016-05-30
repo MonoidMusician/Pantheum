21d9107d621b86fddde9d17afd0942cc959a31ce
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-05-30 15:53:37 -0500

    [PHP5] jPage for Student panel

--- Pages/classes/student.php
+++ Pages/classes/student.php
@@ -19,5 +19,31 @@
 <header>
 	<h1>Class Dashboard</h1>
 </header>
-<article>
-</article>
+<div id="cpage">
+    <nav id="cnav">
+    </nav>
+    <section id="ccontent" class="scrollable">
+    </section>
+</div>
+<div class="clear"></div>
+<script type="text/javascript">
+    $(function() {
+        cpage = new jPage();
+        cpage.init('ccontent');
+
+        var dpage = "";
+        if (location.hash == '') {
+            dpage = '#overview';
+        } else {
+            dpage = location.hash;
+        }
+
+        cpage.setPages([
+            ["#overview", "Overview", "Overview | Student Panel | Pantheum ", "/Pages/class/student-overview.php", true],
+            ["#quizzes", "Quizzes", "Quizzes | Student Panel | Pantheum", "/Pages/class/student-quizzes.php", true],
+            ["#settings", "Settings", "Settings | Student Panel | Pantheum", "/Pages/class/student-settings.php", false]
+        ], dpage);
+        cpage.setNavigation('tnav', 'ul');
+        cpage.load();
+    });
+</script>
